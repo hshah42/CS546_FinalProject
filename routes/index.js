c2eb4181f3f82a97a52bc60fc905e6f6adf2c50d
hshah42 <43765691+hshah42@users.noreply.github.com>
2018-12-02 22:21:32 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 const path = require("path");
 
 const constructorMethod = app => {
-
-  app.get("*", (req, res) => {
+  
+  app.get("/home", (req, res) => {
     res.render("layouts/main");
+  });
+
+  app.get("/login", (req, res) => {
+    res.render("layouts/login");
+  });
+
+  app.get("/createEvents", (req, res) => {
+    res.render("layouts/createEvents");
+  });
+
+  app.get("/signup", (req, res) => {
+    res.render("layouts/signup");
+  });
+
+  app.get("/myprofile", (req, res) => {
+    res.render("layouts/profile");
+  });
+
+  app.get("/event", (req, res) => {
+    res.render("layouts/event");
+  });
+
+  app.get("/events", (req, res) => {
+    res.render("layouts/events");
   });
 };
 
